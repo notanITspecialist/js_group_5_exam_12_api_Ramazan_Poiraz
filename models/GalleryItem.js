17403fe07cbc811319d9942178f1842f739adb73
ramazan <assasins11122001@gmail.com>
2020-04-18 11:57:10 +0600

    Добавил модели

---
+++ models/GalleryItem.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const GalleryItemSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        req: 'user'
+    }
+});
+
+const GalleryItem = mongoose.model('galleryItem', GalleryItemSchema);
+
+module.exports = GalleryItem;
